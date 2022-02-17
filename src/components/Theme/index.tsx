0598d4351ec04fb0019e09c5b08910b0a1d77b35
Daniel Lins <dlins300@gmail.com>
2022-02-17 19:10:25 -0300

    Ajustes finais

--- src/components/Theme/index.tsx
+++ src/components/Theme/index.tsx
@@ -1,61 +1,57 @@
-import { ReactNode } from 'react';
-import * as C from './styles';
-import { Header } from '../../components/Header'
-import { SidebarItem } from '../SidebarItem'
-import { useForm } from '../../contexts/FormContext'
+import { ReactNode } from "react";
+import * as C from "./styles";
+import { Header } from "../../components/Header";
+import { SidebarItem } from "../SidebarItem";
+import { useForm } from "../../contexts/FormContext";
 
 type Props = {
-    children: ReactNode;
-}
-export const Theme = ({children}: Props) => {
+  children: ReactNode;
+};
+export const Theme = ({ children }: Props) => {
+  const { state } = useForm();
 
-    const { state } = useForm();
-    
-    return (
-        <C.Container>
-            <C.Area>
-                <Header />
+  return (
+    <C.Container>
+      <C.Area>
+        <Header />
 
-                <C.Steps>
-                    <C.Sidebar>
-                            <SidebarItem 
-                              title="Pessoal"
-                              description="Se nível"
-                              icon="profile"
-                              path="/"
-                              active={state.currentStep === 1}
-                            />
+        <C.Steps>
+          <C.Sidebar>
+            <SidebarItem
+              title="Pessoal"
+              description="Se nível"
+              icon="profile"
+              path="/"
+              active={state.currentStep === 1}
+            />
 
-                            <SidebarItem 
-                              title="Profissional"
-                              description="Seu nível"
-                              icon="book"
-                              path="/step2"
-                              active={state.currentStep === 2}
-                            />
+            <SidebarItem
+              title="Profissional"
+              description="Seu nível"
+              icon="book"
+              path="/step2"
+              active={state.currentStep === 2}
+            />
 
-                            <SidebarItem 
-                               title="Contatos"
-                              description="Como te achar"
-                             icon="mail"
-                              path="/step3"
-                              active={state.currentStep === 3} 
-                            />
+            <SidebarItem
+              title="Contatos"
+              description="Como te achar"
+              icon="mail"
+              path="/step3"
+              active={state.currentStep === 3}
+            />
 
-                            <SidebarItem 
-                              title="Informações"
-                              description="Suas informações"
-                              icon="info"
-                              path="/step4"
-                              active={state.currentStep === 4}
-                            />
-
-                    </C.Sidebar>
-                    <C.Page>
-                        {children}
-                    </C.Page>
-                </C.Steps>
-            </C.Area>
-        </C.Container>
-    );
+            <SidebarItem
+              title="Informações"
+              description="Suas informações"
+              icon="info"
+              path="/step4"
+              active={state.currentStep === 4}
+            />
+          </C.Sidebar>{" "}
+          <C.Page>{children}</C.Page>
+        </C.Steps>
+      </C.Area>
+    </C.Container>
+  );
 };
